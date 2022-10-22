d1a9c1d02de00cafc7c540ac57eb634d2daf8336
prosite01 <woolfon@mail.ru>
2022-10-22 15:48:25 +0600

    draft

--- src/Get.php
+++ src/Get.php
@@ -8,38 +8,59 @@ class Get
 {
     public static function files($img)
     {
-        $webpFile = null;
         $imgFullPath = Yii::getAlias('@webroot') . $img;
 
         if (file_exists($imgFullPath) === false) {
-            return null;
+            return false;
         }
         
 		$fileSize = filesize($imgFullPath);
 		if ($fileSize === 0) {
-			return null;
+			return false;
 		}
 
 		$fileInfo = pathinfo($imgFullPath);
 		$extension = strtolower($fileInfo['extension']);
-
-        $webpFileName = $fileInfo['filename'] . '.webp';
-		$webpPath = $fileInfo['dirname']  . '/' . $webpFileName;
-
-        if (file_exists($webpPath)) {
-            $webpFile = $webpPath;
+        if (!in_array($extension, ['jpg', 'jpeg', 'png'])) {
+            return static::returnPathsArray($img, false);
         }
 
-        return static::returnPathsArray($img, $webpFile);
+        $webpFileName = $fileInfo['filename'] . '.webp';
+		$webpFullPath = $fileInfo['dirname']  . '/' . $webpFileName;
+        if (file_exists($webpFullPath)) {
+            return static::returnPathsArray($img, true);
+        }
+
+        switch ($extension) {
+            case 'jpg':
+            case 'jpeg':
+                $tmpImg = imagecreatefromjpeg($imgFullPath);
+                imagepalettetotruecolor($tmpImg);
+                imagealphablending($tmpImg, true);
+                imagesavealpha($tmpImg, true);
+                imagewebp($tmpImg, $webpFullPath, 80);
+                imagedestroy($tmpImg);
+                break;
+            case 'png':
+                $tmpImg = imagecreatefrompng($imgFullPath);
+                imagepalettetotruecolor($tmpImg);
+                imagealphablending($tmpImg, true);
+                imagesavealpha($tmpImg, true);
+                imagewebp($tmpImg, $webpFullPath, 80);
+                imagedestroy($tmpImg);
+                break;
+        }
+
+        return static::returnPathsArray($img, true);
     }
 
-    private static function returnPathsArray($img, $webp = false)
+    private static function returnPathsArray($img, $withWebp = false)
     {
         $imgPath = pathinfo($img);
 
         return [
             'original' => $img,
-            'webp' => ($webp === true) ? $imgPath['dirname']  . '/' . $imgPath['filename'] . '.webp' : null
+            'webp' => ($withWebp == true) ? $imgPath['dirname']  . '/' . $imgPath['filename'] . '.webp' : null
         ];
     }
 }
